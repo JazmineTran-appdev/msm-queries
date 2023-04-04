56585524031aa67b7b7c0353d8b86c808ab75645
Jazmine Tran <jazminetra00@gmail.com>
2023-04-04 22:14:41 +0000

    director detail page looks good

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -18,4 +18,14 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/youngest.html.erb"})
   end
+
+  def director_details
+    # params = {"an_id"=>"24"}
+
+    the_id = params.fetch("an_id")
+
+    @the_director = Director.where({ :id => the_id }).at(0)
+
+    render({ :template => "director_templates/show.html.erb"})
+  end
 end
