766ec1ed0d60fd80ecda940f4551b61692f3943e
Jazmine Tran <jazminetra00@gmail.com>
2023-04-04 21:52:44 +0000

    eldest and youngest pages done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -8,7 +8,14 @@ class DirectorsController < ApplicationController
   end
 
   def wisest
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).first
 
     render({ :template => "director_templates/eldest.html.erb"})
+  end
+
+  def young
+    @youngest = Director.where.not({ :dob => nil }).order({ :dob => :desc }).first
+
+    render({ :template => "director_templates/youngest.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index" })
 
   get("/directors/eldest", { :controller => "directors", :action => "wisest" })
+
+  get("/directors/youngest", { :controller => "directors", :action => "young" })
 end
