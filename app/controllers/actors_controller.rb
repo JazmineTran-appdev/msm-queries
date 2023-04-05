09a13b394e0383f5904c2148b4fc0c7207d00dc9
Jazmine Tran <jazminetra00@gmail.com>
2023-04-05 18:05:21 +0000

    fixed actor filmography table now shows every role

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -13,11 +13,6 @@ class ActorsController < ApplicationController
     @actor_id = params.fetch("an_id")
     @actor = Actor.where({ :id => @actor_id }).at(0)
 
-    @character = Character.where({ :actor_id => @actor_id }).at(0)
-
-    @movie = Movie.where({ :id => @character.movie_id }).at(0)
-    @director = Director.where({ :id => @movie.director_id }).at(0)
-
     render({ :template => "actor_templates/actor_details.html.erb"})
   end
 end
